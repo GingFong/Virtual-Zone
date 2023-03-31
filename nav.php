c294cd45f59c4a8edbf1ce689ad0700a31bf54f6
GingFong <90309841+GingFong@users.noreply.github.com>
2023-03-31 13:13:28 +0200

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE.php>
+.php lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -13,17 +13,17 @@
 <body>
 
     <!-- Navbar-->
-  <a href="Homepage.html"><img src="img/logo1.png" class="logo"></a>
+  <a href="Homepage.php"><img src="img/logo1.png" class="logo"></a>
   <section class="nav">
     <nav>
 
       <div class="nav-links" id="navLinks">
         <i class="fa-solid fa-xmark" onclick="hideMenu()"></i>
         <ul>
-          <li><a href="Homepage.html">Home</a></li>
-          <li><a href="library.html">Bibliotheek</a></li>
-          <li><a href="Contact.html">Contact</a></li>
-          <a href="bestellen.html"><img class="cartlogo" src="img/cart.png" alt="cart"></a>
+          <li><a href="Homepage.php">Home</a></li>
+          <li><a href="library.php">Bibliotheek</a></li>
+          <li><a href="Contact.php">Contact</a></li>
+          <a href="bestellen.php"><img class="cartlogo" src="img/cart.png" alt="cart"></a>
           <a href="login.php"><img class="profilelogo" src="img/Profile.png" alt="profilelogo"></a>
         </ul>
       </div>
@@ -39,11 +39,11 @@
         <span></span>
         </label>
         <ul class="hamburger_menu">
-        <li><a class="menu_item" href="index.html">Home</a></li>
-        <li><a class="menu_item" href="projecten.html">Projecten</a></li>
-        <li><a class="menu_item" href="projecten.html">Services</a></li> 
-        <li><a class="menu_item" href="profiel.html">Profiel</a></li> 
-        <li><a class="menu_item" href="inloggen.html">Inloggen</a></li> </ul>
+        <li><a class="menu_item" href="index.php">Home</a></li>
+        <li><a class="menu_item" href="projecten.php">Projecten</a></li>
+        <li><a class="menu_item" href="projecten.php">Services</a></li> 
+        <li><a class="menu_item" href="profiel.php">Profiel</a></li> 
+        <li><a class="menu_item" href="inloggen.php">Inloggen</a></li> </ul>
         
         </nav>
         </header>
